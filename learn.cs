72b75531df08bfcc6222bc1bd4021154f6a9f0e0
Arda Altunel <ardaltunelmain@gmail.com>
2024-02-29 20:49:34 +0300

    Update learn.cs

--- learn.cs
+++ learn.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -409,6 +411,304 @@ namespace ogren
             */
             #endregion
 
+            #region Do While
+            // while döngüsü mekanizmasında olduğu gibi koşula bağlıdır
+            // ilk önce do içerisindeki komut satırları işlenir
+            /*
+            do
+            {
+                // komut satırları
+                // koşula bakmadan çalışır
+            } while (true);
+            */
+
+            // kullanıcıdan 2 sayı isteyelim bu sayıları toplayıp ekrana yazdıralım
+            // yeni bir işlem yapmak istiyor musunuz diye soru yöneltelim
+            // cevap 'e' ise işlem tekrar etsin
+            /*
+            char cevap;
+            int sayi1, sayi2, toplam;
+            do {
+                Console.Clear();
+                Console.WriteLine("2 Sayı Giriniz");
+                Console.Write("İlk Sayı: ");
+                sayi1 = int.Parse(Console.ReadLine());
+                Console.Write("İkinci Sayı: ");
+                sayi2 = int.Parse(Console.ReadLine());
+                toplam = sayi1 + sayi2;
+                Console.WriteLine("Toplama Sonucu: " + toplam);
+                Console.Write("Yeni bir işlem yapmak için 'e' yazınız: ");
+                cevap = char.Parse(Console.ReadLine());
+            } while (cevap == 'e');
+            */
+
+            // kullanıcı adı ve şifre girildikten sonra giriş başarılı denecek
+            // bellirtilen kullanıcı adı ve şifre girilmemiş ise giriş yapılamayacak
+            /*
+            string userName = "arda", password = "1234";
+            string userNameGir, passwordGir;
+            do {
+                Console.Write("Kullanıcı adı: ");
+                userNameGir = Console.ReadLine();
+                Console.Write("Şifre: ");
+                passwordGir = Console.ReadLine();
+
+                if (userNameGir == userName && password == passwordGir) {
+                    Console.Write($"Hoşgeldin, {userNameGir} olarak giriş yaptın.");
+                } else {
+                    Console.WriteLine("Hatalı giriş lütfen tekrar dene\n");
+                }
+            } while (userNameGir != userName && password != passwordGir);
+            */
+
+            // kullancıdan pozitif bir sayı girmesini isteyelim
+            // negeatif sayı girene kadar bu işlemi yaptıralım
+            // eğer negatif bir sayı girer ise sayı istemeyi bırkalım
+            /*
+            int sayi1;
+            do
+            {
+                Console.Write("Pozitif bir sayı giriniz: ");
+                sayi1 = int.Parse(Console.ReadLine());
+                if (sayi1 < 0) {
+                    Console.Write("Hata, bu sayı negatif");
+                } else {
+                    // Console.Write("pozitif");
+                }
+            } while (sayi1 >= 0);
+            */
+
+            // 1 sayı alalım 1 den girmiş olduğu sayıların toplamını yazdıralım
+            /*
+            int i = 1, toplam = 0;
+            Console.Write("Bir Sayı Giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            do {
+                toplam += i;
+                i++;
+            } while (i <= sayi);
+            Console.WriteLine($"1'den {sayi}'e kadar olan sayıların toplamı: {toplam}");
+            */
+            #endregion
+
+            #region SwitchCase
+            // if-else mekanizamasında olduğu gibi karşılaştırma işlemi yapmanızı sağlayan bir mekanizmadır 
+            // yalnız mekanizma içerisinde sadece eşitlik sorgulaması yapılır
+            // if-else gibi komplike sorgular yapılmaz
+            /*
+            switch (switch_on) // deger
+            {
+                default: // yazılması zorunlu degil else gibi
+                    break; // çıkmak için
+            }
+            */
+
+            // 1-12 arası bir sayı alalım
+            // girilen sayıya göre ay bilgisini döndürelim
+            /*
+            Console.Write("1-12 Arası Bir Sayı Giriniz: ");
+            int ay = int.Parse(Console.ReadLine());
+            switch (ay)
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs");
+                    break;
+                case 6:
+                    Console.WriteLine("Haziran");
+                    break;
+                case 7:
+                    Console.WriteLine("Temmuz");
+                    break;
+                case 8:
+                    Console.WriteLine("Ağustos");
+                    break;
+                case 9:
+                    Console.WriteLine("Eylül");
+                    break;
+                case 10:
+                    Console.WriteLine("Ekim");
+                    break;
+                case 11:
+                    Console.WriteLine("Kasım");
+                    break;
+                case 12:
+                    Console.WriteLine("Aralık");
+                    break;
+                default:
+                    Console.WriteLine("Hatalı bir sayı girdiniz");
+                    break;
+            }
+            */
+
+            // 1-12 arası bir sayı isteyelim mevsim bilgisini ekrana yazdıralım
+            /*
+            Console.Write("1-12 Arası Bir Sayı Giriniz: ");
+            int mevsim = int.Parse(Console.ReadLine());
+            switch (mevsim)
+            {
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar");
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış");
+                    break;
+                default:
+                    Console.WriteLine("Hatalı bir sayı girdiniz");
+                    break;
+            }
+            */
+            #endregion
+
+            #region Random Sınıfı
+            // Programımız içerisinde bazı durumlarda rasgele sayı üretmek isteyebilir
+            // Ramdom sayı üretmek için
+            // 0 dan başlayım int veri tipinin max sayı arasındaki rasgele bir sayı üretmek için
+            /*
+            Random rnd = new Random();
+            int rSayi1 = rnd.Next();
+            Console.WriteLine(rSayi1);
+            */
+
+            // 50 ile 100 aralığında random sayı üretmek için
+            /*
+            Random rnd = new Random();
+            int rSayi1 = rnd.Next(50, 101);
+            Console.WriteLine(rSayi1);
+            */
+
+            // 0 ile 1 arasında random sayı üretmek
+            /*
+            Random rnd = new Random();
+            double rSayi1 = rnd.NextDouble();
+            Console.WriteLine(rSayi1);
+            */
+
+            // Sayısal loto çekilişlerine çıkan sayıları erkana yazdıran bir uygulama yazalım
+            /*
+            Random rnd = new Random();
+            int sayac = 1, hedef = 6;
+            while (sayac <= hedef)
+            {
+                Console.Write(rnd.Next(1, 61) + " ");
+                sayac++;
+            }
+            */
+
+            // 1 ile 10 arasında rastgele bir sayı üretelim kullanıcıdan sayıyı tahmin etmesini isteyelim sayıyı bulana kadar
+            /*
+            Random rnd = new Random();
+            int rSayi1 = rnd.Next(1, 11);
+            int tahmin = 0, i = 0;
+            do {
+                Console.Write("Sayıyı Tahmin Et: ");
+                tahmin = int.Parse(Console.ReadLine());
+                i++;
+            } while (tahmin != rSayi1);
+            Console.WriteLine($"Tebrikler, {i} denemede buldunuz");
+            */
+            #endregion
+
+            #region Array
+            // Array(Dizi)
+            // Program yazarkeen bazı durumlarda aynı türden birden fazla değer tutmak zorunda kalabiliriz
+            // Array tanımlama işlemini [] işaretleri arasında yaparız
+            // İçerisinde 5 adet elaman barındıran int türünde bir array oluşturun ve içine elaman atayın
+            /*
+            int[] sayilar = new int[5];
+            sayilar[0] = 5;
+            sayilar[1] = 45;
+            sayilar[2] = 100;
+            sayilar[3] = -25;
+            sayilar[4] = 0;
+
+            Console.WriteLine($"0. indexteki 1. Eleman: {sayilar[0]}");
+            Console.WriteLine($"0. indexteki 2. Eleman: {sayilar[1]}");
+            Console.WriteLine($"0. indexteki 3. Eleman: {sayilar[2]}");
+            Console.WriteLine($"0. indexteki 4. Eleman: {sayilar[3]}");
+            Console.WriteLine($"0. indexteki 5. Eleman: {sayilar[4]}");
+            */
+
+            // Kullanıcıdan 3 adet sayı talep edelim
+            // Sayıların toplamını ve ortalama ve ortalamasını erkana yazdıralım
+            // Değişken tanımlarını Array ile yapalım
+            /*
+            int[] sayilar = new int[3];
+            Console.Write("1. Sayıyı Giriniz: ");
+            sayilar[0] = int.Parse(Console.ReadLine());
+            Console.Write("2. Sayıyı Giriniz: ");
+            sayilar[1] = int.Parse(Console.ReadLine());
+            Console.Write("3. Sayıyı Giriniz: ");
+            sayilar[2] = int.Parse(Console.ReadLine());
+            int toplam = sayilar[0] + sayilar[1] + sayilar[2];
+            Console.WriteLine("\nSayıların Toplamı: " + toplam);
+            double ort = (double) toplam / sayilar.Length;
+            Console.WriteLine("Sayıların Ortalaması: " + ort);
+            */
+
+            // Kullanıcıdan 5 adet sayı talep edelim
+            // Talep edilen sayıların 3'e tam bölünebilenlerini toplamnı ekrana yazdıralım
+            // Array ile yapalım
+            /*
+            int[] sayilar = new int[5];
+            Console.WriteLine("5 adet sayı girilecek");
+            Console.Write("1. sayıyı giriniz: ");
+            sayilar[0] = int.Parse(Console.ReadLine());
+            Console.Write("2. sayıyı giriniz: ");
+            sayilar[1] = int.Parse(Console.ReadLine());
+            Console.Write("3. sayıyı giriniz: ");
+            sayilar[2] = int.Parse(Console.ReadLine());
+            Console.Write("4. sayıyı giriniz: ");
+            sayilar[3] = int.Parse(Console.ReadLine());
+            Console.Write("5. sayıyı giriniz: ");
+            sayilar[4] = int.Parse(Console.ReadLine());
+
+            int toplam = 0;
+            if (sayilar[0] % 3 == 0) {
+                toplam += sayilar[0];
+            }
+            if (sayilar[1] % 3 == 0) {
+                toplam += sayilar[1];
+            }
+            if (sayilar[2] % 3 == 0) {
+                toplam += sayilar[2];
+            }
+            if (sayilar[3] % 3 == 0) {
+                toplam += sayilar[3];
+            }
+            if (sayilar[4] % 3 == 0) {
+                toplam += sayilar[4];
+            }
+
+            Console.WriteLine("3'e tam bölünenlen sayıların toplamları: " + toplam);
+            */
+            #endregion
+
             #region For
             // döngü yapıları adında belli olduğu gibi koşul doğruluğunu sağladığı sürece kendi bloğu içerisindeki komut satırlarını işleyen mekanizmadır
             // uygulama for döngüsüne girdiğine 1.ifade çalışır bu aşamada değişken tanımlama ve değişkene değer atama işlemleri uygulanır sadece ilk döngüde çalışır ve daha sonra çalışmaz
@@ -489,7 +789,7 @@ namespace ogren
             */
             #endregion
 
-            #region For Ile Array Oluşturma
+            #region For İle Array Oluşturma
             // for döngüsü ile daha önceden oluşturulmuş array leri doldurma
             /*
             int[] sayilar = new int[5];
@@ -533,6 +833,95 @@ namespace ogren
             Console.WriteLine("Sayıların Toplamı = " + toplam);
             Console.WriteLine("Sayıların Ortalaması = " + (double)toplam / sayilar.Length);
             */
+            #endregion
+
+            #region Foreach
+            // foreach mekanizması döngüyü sağlaması için bir koleksiyona ihtiyaç duyar
+            // bu koleksiyonun içerisindeki bütün öğeleri tekrar tekrar işleme alır
+            /*
+            int[] sayilar = new int[] {15 , 7 , 45 , 68 , -95 , 251};
+            foreach (var yeniDonguSayilar in sayilar)
+            {
+                Console.WriteLine(yeniDonguSayilar);
+            }
+            */
+
+            // 5 elamanlı bir sayı dizisiniz oluşturup for ile elamanları dolduralım
+            // sonra foreach ile elamanları okuyup yan yana yazdıralım
+            // sayıların toplamını ve ortalamasını gösterelim
+            /*
+            int[] sayilar = new int[5];
+            Console.WriteLine("5 Adet Sayı Giriniz");
+            int toplam = 0;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write((i + 1) + ". Sayı Girin: ");
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Girilen Sayılar: ");
+            foreach (var item in sayilar)
+            {
+                toplam += item;
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nToplamları: " + toplam);
+            Console.WriteLine("Ortalamaları: " + (double) toplam/sayilar.Length);
+            */
+
+            // 5 adet sayı alalım bir diziye kayıt edelim
+            // girdiği sayılardan 3'e tam bölünelenleri ekrana yazdıralım foreach ile
+            /*
+            int[] sayilar = new int[5];
+            Console.WriteLine("5 Adet Sayı Giriniz");
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write((i + 1) + ". Sayı Girin: ");
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("3'e Bölünen Sayılar ==> ");
+            foreach (var item in sayilar)
+            {
+                if (item % 3 == 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+            */
+
+            // bu dizi içerisindeki en uzun ve en kısa isimli sehirleri erkana yazıralım
+            /*
+            string[] sehirler = new string[]
+            {
+                "İstanbul",
+                "Kırklareli",
+                "Yozgat",
+                "İzmir",
+                "Muş",
+            };
+            string enKisa = sehirler[0], enUzun = sehirler[0];
+            int enKucuk = sehirler[0].Length, enBuyuk = sehirler[0].Length;
+            foreach (var item in sehirler)
+            {
+                if (enKucuk > item.Length)
+                {
+                    enKucuk = item.Length;
+                    enKisa= item;
+                }
+                if (enBuyuk < item.Length)
+                {
+                    enBuyuk = item.Length;
+                    enUzun = item;
+                }
+            }
+            Console.WriteLine("En uzun isimli şehir: " + enUzun);
+            Console.WriteLine("En kısa isimli şehir: " + enKisa);
+            */
+            #endregion
+
+            #region Hazır String Methodlar
+            // micrasoftun bize sunmuş olduğu bazı hazır string methodlar bulunmaktadır
+            // bu methodlar sayasinde bir takım işlerimizi kolaylaştırabilirsiniz
+            
             #endregion
 
             Console.ReadKey();
